9b79a177a3270d13ecd5e9edb718d49f148a841b
RutikRC <chavanrutik133@gmail.com>
2024-10-28 19:56:32 +0530

    cors setup

--- src/config/corsSetup.js
+++ src/config/corsSetup.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173', 
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000', 
   // origin: process.env.FRONTEND_URL, 
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   credentials: true, 
